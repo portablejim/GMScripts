38f634e19d7a75e0e45e5eb33990d5ee64d1cb9a
James McKay <james.mckay@trausteknik.com>
2023-09-18 22:10:03 +1000

    Initial commit - Add YT live counter

---
+++ ytLiveCounter.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name     Youtube live uptime counter
+// @version  1
+// @grant    none
+// @match https://www.youtube.com/watch*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.addEventListener('yt-navigate-finish', () =>
+    {
+      if (document.querySelector('.ytp-live-badge') && document.querySelector('.ytp-live-badge').getAttribute('disabled') !== 'true')
+      {
+        let videoEl = document.querySelector('video');
+        let intervalId = setInterval(() => {
+          ((time) => { 
+            let liveUptimeEl = document.getElementById('liveUptime');
+            if(!liveUptimeEl)
+            {
+                // try to create the element
+                console.log('Uptime counter: Trying to add live uptime element');
+                let headingEl = document.querySelector('#title h1');
+                if(headingEl)
+                {
+                    console.log('Uptime counter: Heading found');
+                    let timeCodeEl = document.createElement('p');
+                    timeCodeEl.id = 'liveUptime';
+                    timeCodeEl.style.marginInlineStart = '16px';
+                    timeCodeEl.timeoutId = intervalId;
+                    headingEl.appendChild(timeCodeEl);
+                    liveUptimeEl = timeCodeEl;
+                }
+            }
+            
+            if(liveUptimeEl)
+            {
+                const SECONDS_IN_HOUR = 60*60;
+                if(time >= SECONDS_IN_HOUR)
+                {
+                    liveUptimeEl.innerText = '[From start: ' + Math.floor(time/SECONDS_IN_HOUR).toFixed(0) + ':' + Math.floor((time/60)%60).toFixed(0).padStart(2, '0') + ':' + Math.floor(time % 60).toFixed(0).padStart(2, '0') + ']'; 
+                }
+                else
+                {
+                    liveUptimeEl.innerText = '[From start: ' + Math.floor(time/60).toFixed(0) + ':' + Math.floor(time % 60).toFixed(0).padStart(2, '0') + ']'; 
+                }
+            }
+          })(videoEl.currentTime);
+        }, 200);
+      }
+      else
+      {
+        console.log('Uptime counter: Not a live video');
+      }
+    });
+})();
